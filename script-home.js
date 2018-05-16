9d326e85b3b17cf68a9519f7a3fd3c37d29140d6
Haitong32 <zhenh522@newschool.edu>
2018-05-16 12:18:29 +0800

    animation update1

---
+++ script-home.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+     $('#dot').click(function(){
+     $("#dot").hide();
+     $('body').jGravity({
+          target: 'everything',
+          // ignoreClass: 'ignoreMe',
+          weight: 20,
+          depth: 10,
+          drag: true
+     });
+  });
+});
