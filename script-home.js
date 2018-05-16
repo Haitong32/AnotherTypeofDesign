980bc23580f5c1e8b0a775b6cf1c71c601824d02
Haitong32 <zhenh522@newschool.edu>
2018-05-16 16:53:38 +0800

    animation

--- script-home.js
+++ script-home.js
@@ -6,27 +6,5 @@ $(document).ready(function(){
           depth: 10,
           // drag: true
      });
-     var randPercent = Math.random() * 100;
-    var randX = Math.random() * 100;
-    var randY = Math.random() * 100;
-    var randX2 = Math.random() * 100;
-    var randY2 = Math.random() * 100;
-    var randX3 = Math.random() * 100;
-    var randY3 = Math.random() * 100;
-    $('#ling2').click(function(){
-      $('#ling2').css('left', randX + "%");
-      $('#ling2').css('top', randY + "%");
+
     });
-    $('#yi').click(function(){
-      $('#yi').css('left', randX2 + "%");
-      $('#yi').css('top', randY2 + "%");
-    });
-
-    $('#zhong').click(function(){
-      $('#zhong').css('left', randX3 + "%");
-      $('#zhong').css('top', randY3 + "%");
-    });
-
-
-
-  });
