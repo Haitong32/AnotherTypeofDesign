c5b9f4a2a6042c70146d5f5b89be05a7a7b28241
Haitong32 <zhenh522@newschool.edu>
2018-05-16 12:50:35 +0800

    update2

--- script-home.js
+++ script-home.js
@@ -1,12 +1,13 @@
 $(document).ready(function(){
-     $('#dot').click(function(){
-     $("#dot").hide();
+     // $('#dot').click(function(){
+     // $("#dot").hide();
      $('body').jGravity({
           target: 'everything',
           // ignoreClass: 'ignoreMe',
           weight: 20,
           depth: 10,
-          drag: true
+          // drag: true
      });
+     $("text").draggable();
   });
-});
+// });
