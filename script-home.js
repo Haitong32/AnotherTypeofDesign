a573e840dad06507c07243b2388a3fa1bf5b7a8a
Haitong32 <zhenh522@newschool.edu>
2018-05-16 15:31:19 +0800

    js try2

--- script-home.js
+++ script-home.js
@@ -6,4 +6,14 @@ $(document).ready(function(){
           depth: 10,
           // drag: true
      });
+     var div = document.getElementById('ling2');
+    div.addEventListener('touchmove', function(event) {
+    event.preventDefault();//阻止其他事件
+    // 如果这个元素的位置内只有一个手指的话
+    if (event.targetTouches.length == 1) {
+        var touch = event.targetTouches[0];  // 把元素放在手指所在的位置
+        div.style.left = touch.pageX + 'px';
+        div.style.top = touch.pageY + 'px';
+    }
+}, false);
   });
