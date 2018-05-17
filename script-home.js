67bad30c9c00a5d3e569788f90d78a1abc1ee902
Haitong32 <zhenh522@newschool.edu>
2018-05-17 14:41:03 +0800

    try

--- script-home.js
+++ script-home.js
@@ -1,10 +1,53 @@
 $(document).ready(function(){
-  // $('body').jGravity({
-  //      target: 'everything',
-  //      weight: 20,
-  //      depth: 10,
-  //      drag: true
-  // });
+  $('body').jGravity({
+       target: 'everything',
+       weight: 20,
+       depth: 10,
+       drag: true
+  });
 
-  $(".text").draggable();
-    });
+  var proto =  $.ui.mouse.prototype,
+  _mouseInit = proto._mouseInit;
+
+  $.extend( proto, {
+      _mouseInit: function() {
+          this.element
+          .bind( "touchstart." + this.widgetName, $.proxy( this, "_touchStart" ) );
+          _mouseInit.apply( this, arguments );
+      },
+
+      _touchStart: function( event ) {
+           this.element
+          .bind( "touchmove." + this.widgetName, $.proxy( this, "_touchMove" ) )
+          .bind( "touchend." + this.widgetName, $.proxy( this, "_touchEnd" ) );
+
+          this._modifyEvent( event );
+
+          $( document ).trigger($.Event("mouseup")); //reset mouseHandled flag in ui.mouse
+          this._mouseDown( event );
+
+          //return false;
+      },
+
+      _touchMove: function( event ) {
+          this._modifyEvent( event );
+          this._mouseMove( event );
+      },
+
+      _touchEnd: function( event ) {
+          this.element
+          .unbind( "touchmove." + this.widgetName )
+          .unbind( "touchend." + this.widgetName );
+          this._mouseUp( event );
+      },
+
+      _modifyEvent: function( event ) {
+          event.which = 1;
+          var target = event.originalEvent.targetTouches[0];
+          event.pageX = target.clientX;
+          event.pageY = target.clientY;
+      }
+
+  });
+
+});  
