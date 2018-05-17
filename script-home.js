c531260643ab4bf084e6a3cf8dc8a569dc01fa38
Haitong32 <zhenh522@newschool.edu>
2018-05-17 10:28:23 +0800

    update2

--- script-home.js
+++ script-home.js
@@ -1,10 +1,8 @@
 $(document).ready(function(){
      $('body').jGravity({
           target: 'everything',
-          // ignoreClass: 'ignoreMe',
           weight: 20,
           depth: 10,
-          // drag: true
      });
      interact('.draggable')
   .draggable({
